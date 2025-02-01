43d542e09c8ae49aae177a7cdea6a4d586f2fd34
victor alvarez rojas <victoralvarezrojas@MacBook-Air-de-victor.local>
2025-02-01 14:00:18 -0300

    update content

--- src/main/java/org/example/sec04/Lec01FluxCreate.java
+++ src/main/java/org/example/sec04/Lec01FluxCreate.java
@@ -1,7 +1,16 @@
 package org.example.sec04;
 
+import com.github.javafaker.Faker;
+import org.example.common.Util;
+import reactor.core.publisher.Flux;
+
 public class Lec01FluxCreate {
     public static void main(String[] args) {
-
+        Flux.create(fluxSink -> {
+            for (int x = 0; x < 10; x++) {
+                fluxSink.next(new Faker().pokemon().name());
+            }
+            fluxSink.complete();
+        }).subscribe(Util.subscriber());
     }
 }
